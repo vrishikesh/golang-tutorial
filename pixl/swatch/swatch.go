40f0f70b19d96cbc1d2f4f67a74d821a92c94120
Rishikesh Vishwakarma <rishikesh.vishwakarma@m.pegasusinfocorp.com>
2022-05-30 22:55:07 +0530

    save

---
+++ pixl/swatch/swatch.go
@@ -0,0 +1,20 @@
+package swatch
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/widget"
+)
+
+type Swatch struct {
+	widget.BaseWidget
+	Selected     bool
+	Color        color.Color
+	SwatchIndex  int
+	clickHandler func(s *Swatch)
+}
+
+func (s *Swatch) SetColor(c color.Color) {
+	s.Color = c
+	s.Refresh()
+}
